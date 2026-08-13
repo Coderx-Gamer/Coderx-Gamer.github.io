7229fda62bc188a341a164106e4aa89e88e097cb
MrBreakNFix <73456238+MrBreakNFix@users.noreply.github.com>
2026-08-13 13:09:23 -0700

    I don't like the reputation this mod gets. Hopefully this is a step in the right direction.

--- consent.js
+++
@@ -1,8 +0,0 @@
-function acceptCookies() {
-    document.getElementById("cookieConsent").style.display = "none";
-    localStorage.setItem("cookiesAccepted", "true");
-}
-
-if (!localStorage.getItem("cookiesAccepted")) {
-    document.getElementById("cookieConsent").style.display = "block";
-}
